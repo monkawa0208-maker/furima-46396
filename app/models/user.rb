3221497552ce219018b38fdbd5511efffc4ff2c3
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-23 15:39:53 +0900

    パスワードのバリデーション設定
    パスワード：必須・6文字以上・半角英数字混合

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+\z/ }
     validates :nickname
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
