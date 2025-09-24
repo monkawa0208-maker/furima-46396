3bcca0a79bdefa2d65a9137099ea6bc8201355ea
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-24 20:49:10 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,12 +2,11 @@ const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     const profitDom = document.getElementById("profit");
     const tax_rate = 0.1;
     addTaxDom.innerHTML = Math.floor(inputValue * tax_rate);
-    profitDom.innerHTML = Math.floor(inputValue * (1 - tax_rate));
+    profitDom.innerHTML = inputValue - Math.floor(inputValue * tax_rate);
   })
 };
 
