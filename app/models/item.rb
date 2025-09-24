dac06ddec33b5b70de18d75c9172017783e812a6
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-24 16:06:44 +0900

    itemモデルおよび関連ActiveHashモデルを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,38 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  belongs_to :shipping_fee
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_day_id
+    validates :prefecture_id
+    validates :price
+  end
+
+  # '---' (id:1) を弾く
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_day_id
+    validates :prefecture_id
+  end
+
+  # 価格の形式・範囲
+  validates :price,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999
+            }
 end
