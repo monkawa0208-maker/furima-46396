00f0bf621aecaccde107701a8dc421fda3bc6168
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-24 20:25:55 +0900

    itemモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph(sentence_count: 2) }
+    category_id { Faker::Number.between(from: 2, to: 10) }
+    status_id { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    shipping_day_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
