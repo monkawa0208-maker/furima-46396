fb60550e7fd4bf520fb720e2bb5204f97f277ed6
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-24 20:09:14 +0900

    create後、top pageに遷移するように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to item_path(@item)
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
