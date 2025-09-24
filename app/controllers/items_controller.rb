e0b9eefb2fda17823f57d4e479ba7e975866d8b4
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-24 17:56:07 +0900

    new・createアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,40 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :description,
+      :category_id, :status_id, :shipping_fee_id, :shipping_day_id, :prefecture_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
 end
