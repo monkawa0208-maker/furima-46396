4287fe8ff0dbc5409f0b93967124ed6100ea6c7e
Yuji Monkawa <monkawa0208@gmail.com>
2025-09-28 18:03:19 +0900

    orderコントローラーの設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def index
+    @order_shipping = OrderShipping.new
+  end
+
+  def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_shipping).permit(
+      :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+end
